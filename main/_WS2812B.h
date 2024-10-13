6f4996a6104ad718686b495538851c8404963e2b
hekciu <jakublasecki321@gmail.com>
2024-10-13 23:48:22 +0200

    wrote a bit of leds protocol but this thing needs to be faster to work properly

--- main/_WS2812B.h
+++ main/_WS2812B.h
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include "driver/gpio.h"
+#include <rom/ets_sys.h>
 
+/*
+    Write blob of 24 bits to output in order that makes is ready for transmitting
+*/
 void createDataPackage(uint8_t R, uint8_t G, uint8_t B, int * output) {
     uint8_t colors[3] = { G, R, B };
     int outputBitIndex = 0;
@@ -10,7 +17,7 @@ void createDataPackage(uint8_t R, uint8_t G, uint8_t B, int * output) {
 
         uint8_t colorRes = 0;
         for (int colorBit = 0; colorBit < 8; colorBit++) {
-            uint8_t currentMultiplier = (uint8_t)1 << 7 - colorBit;             
+            uint8_t currentMultiplier = (uint8_t)1 << (7 - colorBit);             
             int bit = (color - colorRes) / currentMultiplier;
             *(output + outputBitIndex) = bit;
             colorRes += bit * currentMultiplier;
@@ -18,4 +25,32 @@ void createDataPackage(uint8_t R, uint8_t G, uint8_t B, int * output) {
             outputBitIndex++;
         } 
     }
+}
+
+
+/*
+    Functions sendOne, sendZero and sendReset are designed to be used inside FreeRTOS's tasks
+*/
+
+// TODO: implement faster transition https://www.reddit.com/r/esp32/comments/f529hf/results_comparing_the_speeds_of_different_gpio/?rdt=64373
+#define T0H_us 
+void sendOne(gpio_num_t pin_num) {
+    gpio_set_level(pin_num, 1);
+    ets_delay_us(T0H_us);
+    gpio_set_level(pin_num, 0);
+    ets_delay_us(T0L_us);
+}
+
+
+void sendZero(gpio_num_t pin_num) {
+    gpio_set_level(pin_num, 1);
+    ets_delay_us(T1H_us);
+    gpio_set_level(pin_num, 0);
+    ets_delay_us(T1L_us);
+}
+
+
+void sendReset(gpio_num_t pin_num) {
+    gpio_set_level(pin_num, 0);
+    ets_delay_us(TReset_us);
 }
--- main/main.c
+++ main/main.c
@@ -6,10 +6,49 @@
 #include "esp_chip_info.h"
 #include "esp_flash.h"
 #include "esp_system.h"
+#include "driver/gpio.h"
 
-#include <FastLED.h>
+#include "_WS2812B.h"
+
+#define OUTPUT_PIN GPIO_NUM_18
+#define GPIO_BIT_MASK (1ULL<<OUTPUT_PIN) 
+#define NUM_LEDS 10
+
+void flash_red() {
+    int * output = malloc(24);
+    printf("flashin red on pin: %d\n", OUTPUT_PIN);
+    createDataPackage(255, 0, 0, output); 
+
+    sendReset();
+
+    for (int i = 0; i < NUM_LEDS; i++) {
+        for (int nBit = 0; nBit < 24; nBit++) {
+            if (*(output + nBit) == 0) {
+                sendZero(); 
+            } else {
+                sendOne();
+            }
+        } 
+    }
+
+    free(output);
+}
 
 void app_main(void)
 {
-    printf("Hello world!\n");
+    printf("starting\n");
+   
+    // configure gpio for communication with WS2812B
+    gpio_config_t ioConf = {};
+    ioConf.intr_type = GPIO_INTR_DISABLE;
+    ioConf.mode = GPIO_MODE_OUTPUT;
+    ioConf.pull_down_en = 0;
+    ioConf.pull_up_en = 0;
+    ioConf.pin_bit_mask = GPIO_BIT_MASK;
+    gpio_config(&ioConf);
+
+    for(;;) {
+        vTaskDelay(1000);
+        flash_red(); 
+    }    
 }
