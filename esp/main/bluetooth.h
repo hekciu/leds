d107a59088df85e2fcd4761348b82f49e818809d
hekciu <jakublasecki321@gmail.com>
2024-11-17 14:12:20 +0100

    remove sdkconfig from version control

--- esp/main/bluetooth.h
+++ esp/main/bluetooth.h
@@ -1,6 +1,7 @@
 #include "esp_bt.h"
 #include "esp_bt_main.h"
-#include "esp_bt_device.h"
 #include "esp_gap_bt_api.h"
+#include "esp_bt_device.h"
+#include "esp_spp_api.h"
 
 // We want bluetooth SERVER - esp32 will wait for commands from mobile device
