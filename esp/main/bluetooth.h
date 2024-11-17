9fced204471ee38203e719f1d03f7335e144a058
hekciu <jakublasecki321@gmail.com>
2024-11-17 14:53:07 +0100

    set up env for ble on esp32s3

--- esp/main/bluetooth.h
+++ esp/main/bluetooth.h
@@ -1,7 +1,28 @@
 #include "esp_bt.h"
+#include "esp_gap_ble_api.h"
+#include "esp_gatts_api.h"
+#include "esp_bt_defs.h"
 #include "esp_bt_main.h"
-#include "esp_gap_bt_api.h"
-#include "esp_bt_device.h"
-#include "esp_spp_api.h"
+#include "esp_gatt_common_api.h"
 
-// We want bluetooth SERVER - esp32 will wait for commands from mobile device
+/*
+    Inspired by:
+    https://github.com/espressif/esp-idf/blob/v5.2.3/examples/bluetooth/bluedroid/ble/ble_spp_server/main/ble_spp_server_demo.c
+*/
+
+
+#define GATTS_TABLE_TAG  "GATTS_HEKCIU_LEDS"
+
+#define SPP_PROFILE_NUM             1
+#define SPP_PROFILE_APP_IDX         0
+#define ESP_SPP_APP_ID              0x56
+#define SAMPLE_DEVICE_NAME          "ESP_HEKCIU_LEDS"    //The Device Name Characteristics in GAP
+#define SPP_SVC_INST_ID	            0
+
+/// SPP Service
+static const uint16_t spp_service_uuid = 0xABF0;
+/// Characteristic UUID
+#define ESP_GATT_UUID_SPP_DATA_RECEIVE      0xABF1
+#define ESP_GATT_UUID_SPP_DATA_NOTIFY       0xABF2
+#define ESP_GATT_UUID_SPP_COMMAND_RECEIVE   0xABF3
+#define ESP_GATT_UUID_SPP_COMMAND_NOTIFY    0xABF4
