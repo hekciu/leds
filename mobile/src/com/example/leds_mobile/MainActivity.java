5f461e6845e6127d395b80c4f636ba1974f466e5
hekciu <jakublasecki321@gmail.com>
2024-11-21 21:28:06 +0100

    fun with UI in java (it is bad)

--- mobile/src/com/example/leds_mobile/MainActivity.java
+++ mobile/src/com/example/leds_mobile/MainActivity.java
@@ -5,19 +5,52 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.view.View;
+import android.graphics.Color;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinearLayout backgroundLayout = findViewById(R.id.backgroundLayout);
+
+        int red = 0x50FF0000;
+        int green = 0x5000FF00;
+        int blue = 0x500000FF;
+        int purple = 0x50FF00FF;
+        int backgroundColor = 0x50808080;
 
         Button buttonRed = findViewById(R.id.buttonRed);
+        buttonRed.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+               backgroundLayout.setBackgroundColor(red);
+            }
+        });
         Button buttonBlue = findViewById(R.id.buttonBlue);
+        buttonBlue.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+               backgroundLayout.setBackgroundColor(blue);
+            }
+        });
         Button buttonGreen = findViewById(R.id.buttonGreen);
+        buttonGreen.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+               backgroundLayout.setBackgroundColor(green);
+            }
+        });
         Button buttonPurple = findViewById(R.id.buttonPurple);
+        buttonPurple.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+               backgroundLayout.setBackgroundColor(purple);
+            }
+        });
+        Button buttonReset = findViewById(R.id.buttonReset);
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+               backgroundLayout.setBackgroundColor(backgroundColor);
+            }
+        });
 
-        LinearLayout backgroundLayout = findViewById(R.id.backgroundLayout);
-        backgroundLayout.setBackgroundColor(Color.GREY);
+        backgroundLayout.setBackgroundColor(backgroundColor);
     }
 }
